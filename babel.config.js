8c213fa3809761912327859fadfa09dfa72eac1d
Alexander Kiyko <55935063+alexkiyko@users.noreply.github.com>
2020-08-28 01:29:57 -0400

    configs setup (#2)

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    presets: [
-        ['@babel/preset-env', {
-            targets: {
-                node: 12
-            }
-        }]
-    ]
-}
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 12,
+        },
+      },
+    ],
+  ],
+};
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,1 +1,6 @@
-
+describe('', function () {
+  it('should ', function () {
+    browser.url(`${browser.options.baseUrl}/user/login`);
+    browser. pause(5000);
+  });
+});
