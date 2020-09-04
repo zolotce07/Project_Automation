b80a77e640725f7b30577f1da9e8491c89454cee
alexkiyko <avkiyko@gmail.com>
2020-09-04 10:50:18 -0400

    refactor utils

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,19 +1,46 @@
-
 class Utils {
-
-  verifyElementText(element, text) {
-    browser.waitUntil(() => element.getText() === text);
+  setValue(element, value) {
+    this.waitForDisplayed(element);
+    element.setValue(value);
   }
 
-  verifyElementIsDisplayed(element) {
-    browser.waitUntil(() => element.isDisplayed());
+  getText(element) {
+    this.waitForDisplayed(element);
+    element.getText();
   }
 
-  isClickableBtn(element) {
-    browser.waitUntil(() => element.isClickable());
+  click(element) {
+    this.waitForEnabled(element);
     element.click();
+  }
+
+  waitForEnabled(element) {
+    this.waitForDisplayed(element);
+    element.waitForEnabled({
+      timeout: 5000,
+      timeoutMsg: `Element not enabled - '${element.selector}', Current page: ${browser.getUrl()}`,
+    });
+  }
+
+  waitForDisplayed(element) {
+    this.isExist(element);
+    element.waitForDisplayed({
+      timeout: 5000,
+      timeoutMsg: `Element not visible - '${element.selector}', Current page: ${browser.getUrl()}`,
+    });
+  }
+
+  isDisplayed(element) {
+    this.waitForDisplayed(element);
+    return element.isDisplayed();
+  }
+
+  isExist(element) {
+    element.waitForExist({
+      timeout: 5000,
+      timeoutMsg: `Element not exist - '${element.selector}', Current page: ${browser.getUrl()}`,
+    });
   }
 }
 
 export default new Utils();
-
