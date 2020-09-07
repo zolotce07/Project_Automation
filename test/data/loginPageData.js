3116f12a3c609be27b71e2c610b8ba6274e763c3
Alexander Kiyko <avkiyko@gmail.com>
2020-09-06 23:53:29 -0400

    create loginPageData.js

---
+++ test/data/loginPageData.js
@@ -0,0 +1,18 @@
+const loginPageData = {
+  header: 'Welcome back!',
+  registerAccount : {
+    description: 'Don’t have an account?',
+    link: 'Create one',
+  },
+  forgotPassword : {
+    description: 'Forgot your password?',
+    link: 'Reset it'
+  },
+  errorMsg : {
+    invalidEmail: 'email is not a valid email',
+    required: 'Required'
+  },
+  loginBtn: 'Log in'
+};
+
+module.exports = loginPageData;
