e598fa143f27cb51d10244d4e0e6b9a6e5194dbb
stozhkairyna <stsirasts@gmail.com>
2020-09-17 15:03:51 -0500

    smoke/settingsProfile.spec.js utils/fieldEmptyfier

--- test/specs/smoke/userSettingProfile.spec.js
+++ test/specs/smoke/userSettingProfile.spec.js
@@ -1,6 +1,19 @@
 import settingsProfile from '../../pageObject/settingsProfile';
+import loginPage from '../../pageObject/loginPage';
+import { registeredUser } from '../../data/userData';
+import userProfilePage from '../../pageObject/userProfilePage';
 
-describe('', ()=> {
+describe('USER PROFILE PERSONAL INFO FILLING/UPDATE', () => {
+  before('', () => {
+    loginPage.open();
+    loginPage.login(registeredUser);
+    userProfilePage.goToSetting()
+  });
   it('', () => {
+    settingsProfile.userSettingsProfileInput();
   });
 });
+
+
+// wdio wdio.conf.js --spec ./test/specs/smoke/userSettingProfile.spec.js
+// this.settingsProfilePhone.keys('Backspace'.repeat(5));
