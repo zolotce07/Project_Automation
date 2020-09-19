39c5baa57e2bdb5f4deb13f86295ab696a89b7a1
Alexander Kiyko <avkiyko@gmail.com>
2020-09-19 02:01:53 -0400

    add login.spec negative test, login data refactor, login page error msg locator

--- test/specs/smoke/userLogIn.spec.js
+++ test/specs/smoke/userLogIn.spec.js
@@ -1,42 +1,91 @@
 import { expect } from 'chai';
 import LoginPage from '../../pageObject/loginPage';
-import { registeredUser, admin } from '../../data/userData';
-import Utils from '../../helpers/utils';
-import UserProfilePage from '../../pageObject/userProfilePage';
 import AdminProfilePage from './../../pageObject/adminProfilePage';
+import UserProfilePage from '../../pageObject/userProfilePage';
+import Utils from '../../helpers/utils';
 import loginPageData from './../../data/loginPageData';
+import { registeredUser, admin } from '../../data/userData';
 
-describe('USER LOG IN AS USER/ADMIN', () => {
-  before('Open Login Page', () => {
-    LoginPage.open();
-    expect(browser.getUrl()).includes('/user/login');
+describe('LOGIN FUNCTIONALITY', () => {
+  describe('POSITIVE - USER LOG IN / LOG OUT AS USER / ADMIN', () => {
+    before('Open Login Page', () => {
+      LoginPage.open();
+      expect(browser.getUrl()).includes('/user/login');
+    });
+
+    it('Login As Admin', () => {
+      LoginPage.login(admin);
+      Utils.waitForDisplayed(AdminProfilePage.menuBtn);
+      expect(Utils.getText(AdminProfilePage.accountHolderName)).eq(
+        `${admin.firstName} ${admin.lastName}`,
+      );
+    });
+
+    it('Admin Logout', () => {
+      AdminProfilePage.logout();
+      expect(LoginPage.header.getText()).eq(loginPageData.header);
+    });
+
+    it('Login As registeredUser', () => {
+      LoginPage.login(registeredUser);
+      Utils.waitForDisplayed(UserProfilePage.menuBtn);
+      expect(Utils.getText(UserProfilePage.accountHolderName)).eq(
+        `${registeredUser.firstName} ${registeredUser.lastName}`,
+      );
+    });
+
+    it('registeredUser Logout', () => {
+      UserProfilePage.logout();
+      expect(LoginPage.header.getText()).eq(loginPageData.header);
+    });
   });
 
-  it('Login As Admin', () => {
-    LoginPage.login(admin);
-    Utils.waitForDisplayed(AdminProfilePage.menuBtn);
-    expect(Utils.getText(AdminProfilePage.accountHolderName)).eq(
-      `${admin.firstName} ${admin.lastName}`,
-    );
+  describe('NEGATIVE - ADMIN LOGIN', () => {
+    beforeEach('Open Login Page', () => {
+      LoginPage.open();
+      expect(browser.getUrl()).includes('/user/login');
+    });
+
+    it('Verify user should not login with correct email and empty password', () => {
+      Utils.setValue(LoginPage.emailInput, admin.email);
+      Utils.setValue(LoginPage.passwordInput, '');
+      expect(LoginPage.loginBtn.isEnabled()).to.be.false;
+    });
+
+    it('Verify user should not login with correct password and empty email', () => {
+      Utils.setValue(LoginPage.emailInput, '');
+      Utils.setValue(LoginPage.passwordInput, admin.password);
+      expect(LoginPage.loginBtn.isEnabled()).to.be.false;
+    });
+
+    it('Verify user should not login with empty email and empty password', () => {
+      Utils.setValue(LoginPage.emailInput, '');
+      Utils.setValue(LoginPage.passwordInput, '');
+      expect(LoginPage.loginBtn.isEnabled()).to.be.false;
+    });
   });
 
-  it('Logout', () => {
-    AdminProfilePage.logout();
-    expect(LoginPage.header.getText()).eq(loginPageData.header);
-  });
+  describe('NEGATIVE - ERROR MSG', () => {
+    beforeEach('Open Login Page', () => {
+      LoginPage.open();
+      expect(browser.getUrl()).includes('/user/login');
+    });
 
-  it('Login As registeredUser', () => {
-    LoginPage.login(registeredUser);
-    Utils.waitForDisplayed(UserProfilePage.menuBtn);
-    expect(Utils.getText(UserProfilePage.accountHolderName)).eq(
-      `${registeredUser.firstName} ${registeredUser.lastName}`,
-    );
-  });
+    it('Verify email error is displayed (input - invalid email)', () => {
+      Utils.setValue(LoginPage.emailInput, 'sdf@');
+      expect(Utils.getText(LoginPage.errorMsg)).eq(loginPageData.errorMsg.invalidEmail);
+    });
 
-  it('Logout', () => {
-    UserProfilePage.logout();
-    expect(LoginPage.header.getText()).eq(loginPageData.header);
+    it('Verify email error is displayed (input - empty email filed)', () => {
+      Utils.setValue(LoginPage.emailInput, 'aljoi');
+      Utils.fieldEmptyfier(LoginPage.emailInput);
+      expect(Utils.getText(LoginPage.errorMsg)).eq(loginPageData.errorMsg.required);
+    });
+
+    it('Verify password error is displayed (input - empty password filed)', () => {
+      Utils.setValue(LoginPage.passwordInput, 'ytdu6546');
+      Utils.fieldEmptyfier(LoginPage.passwordInput);
+      expect(Utils.getText(LoginPage.errorMsg)).eq(loginPageData.errorMsg.required);
+    });
   });
 });
-
-// wdio wdio.conf.js --spec ./test/specs/smoke/userLogIn.spec.js// to run only single file
