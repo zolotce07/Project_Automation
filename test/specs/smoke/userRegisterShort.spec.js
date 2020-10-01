94fd88376276da491b7c70061068b359cd24386a
Artem Nikolaev <58619990+nikolaev-a@users.noreply.github.com>
2020-10-01 06:36:12 +0300

    random user delete (#43)

--- test/specs/smoke/userRegisterShort.spec.js
+++ test/specs/smoke/userRegisterShort.spec.js
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import RegisterPage from '../../pageObject/registerPage';
 import UserProfilePage from './../../pageObject/userProfilePage';
 import Utils from '../../helpers/utils';
-import { randomUser } from './../../data/userData';
+import { admin, randomUser } from './../../data/userData';
+import userDataAPI from '../../helpers/hooks';
+const axios = require('axios');
 
 describe('USER REGISTER SHORT', () => {
   before('Open Register Page', () => {
@@ -15,6 +17,40 @@ describe('USER REGISTER SHORT', () => {
     expect(Utils.getText(UserProfilePage.accountHolderName)).eq(
       `${randomUser.firstName} ${randomUser.lastName}`,
     );
+  });
+
+  it('should user be registered ', async () => {
+    const rmUser = await axios({
+      method: 'post',
+      url: 'https://server-stage.pasv.us/user/login',
+      data: {
+        email: randomUser.email,
+        password: randomUser.password,
+      },
+    })
+      .then(res => res)
+      .then(err => err);
+    expect(rmUser.data.user.name).eq(randomUser.firstName +' '+ randomUser.lastName);
+  });
+
+  it('should return admin token', async () => {
+    const dataAPIAdmin = await userDataAPI(admin);
+    process.env.TOKEN_ADMIN = dataAPIAdmin.data.token;
+  });
+
+  it('should random user delete', async () => {
+    const randomUserDelete = await axios({
+      method: 'delete',
+      url: `https://server-stage.pasv.us/user/email/${randomUser.email}`,
+      headers: {
+        Authorization: process.env.TOKEN_ADMIN,
+      },
+    })
+      .then(res => res)
+      .catch(err => err);
+    console.log('RESULT ' + randomUserDelete);
+    //expect(dailyReportDelete.data.message).eq('Diary deleted');
+    expect(randomUserDelete.status).eq(200);
   });
 });
 
